7655bcc48a05b2eba9e17d36ba2d54cf73a959ff
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-03-12 10:46:20 +0500

    Everything is set but it has some glitches

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articlesRouter = require('./routes/articlesRoute')
+const methodOverride = require('method-override')
+require('dotenv').config()
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to Node js");
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req,res)=>{
+  const articles = await Article.find().sort({createdAt:'desc'})
+  res.render("articles/index",{articles : articles});
 })
 
-app.listen(404);
+app.use('/articles',articlesRouter)
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri)
+  .then(() => {
+    console.log('Connected to Atlas ...');
+    app.listen(404,()=>{
+        console.log('Server Running at Port 404');
+     })
+}
+).catch((e)=> console.log('Connection Failed!',e));
